0dadb79e8c0a883e793215e2903a02e871e039d3
asics <asics28@yandex.ru>
2022-10-20 12:40:41 +0300

    Решил ДЗ 6

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Сколько чисел вы хотите ввести?"); // ввод размера массива
+int M = int.Parse(Console.ReadLine());
+int [] array = new int[M];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите {i}-ое число: "); // последовательный ввод элементов массива
+    array[i] = int.Parse(Console.ReadLine());
+}
+void Print (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < (array.Length - 1))
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }   
+    }
+}
+Console.Write("Введенные числа: "); Print(array); Console.WriteLine();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+        if (array[i] > 0)
+        {
+            count = count + 1;
+        }
+
+}
+Console.WriteLine($"Среди введенных чисел {count} > 0");
---
+++ Zadacha43/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями:
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+float [,] array = new float [2, 2];
+Console.Write("Введите значение k1: "); array[0, 0] = float.Parse(Console.ReadLine());
+Console.Write("Введите значение k2: "); array[1, 0] = float.Parse(Console.ReadLine());
+array[0, 1] = -1; //коэфициент при y1
+array[1, 1] = -1; //коэфициент при y2
+Console.Write("Введите значение b1: "); float b1 = float.Parse(Console.ReadLine());
+Console.Write("Введите значение b2: "); float b2 = float.Parse(Console.ReadLine());
+
+// int h = array.GetLength(0); // просто показать, что Я умею печатать двумерный массив
+// int w = array.GetLength(1);
+// for (int i =0; i < h; i++)
+// {
+//    for (int j =0; j < w; j++)
+//    {
+//       Console.Write(array[i, j] + "\t");
+//    }
+//    Console.WriteLine();
+// }
+
+float delta = ((array[0, 0] * array[1, 1])-(array[1, 0] * array[0, 1])); // нахождение главного определителя
+float deltaX = (((-b1) * array[0, 1])-((-b2) * array[1, 1])); // нахождение X определителя
+float deltaY = (array[0, 0] * (-b2))-(array[1, 0] * (-b1)); // нахождение Y определителя
+float x = (deltaX/delta);
+float y = (deltaY/delta);
+if (delta != 0)
+{
+    Console.WriteLine("Прямые пересекаются!");
+    Console.Write($"b1 = {b1}, k1 = {array[0, 0]}, b2 = {b2}, k2 = {array[1, 0]}, -> ({x}, {y})");
+}
+if (delta == 0 & deltaX != 0 & deltaY != 0)
+{
+    Console.WriteLine("Прямые параллельны!");
+}
+if (delta == deltaX & delta == deltaY)
+{
+    Console.WriteLine("Прямые совпадают");
+}
